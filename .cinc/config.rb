700a0e6f0ffa3dbda7e45ebf6223b86298dee99c
Edmund Haselwanter <ehaselwanter@infralovers.com>
2025-07-18 14:40:43 +0200

    Update config.rb

--- .cinc/config.rb
+++ .cinc/config.rb
@@ -2,9 +2,9 @@ current_dir = File.dirname(__FILE__)
 userid = ENV['USER_ID']
 username = "tool-user-#{userid}"
 
-log_level                :info
+#log_level                :info
 log_location             STDOUT
 node_name                username
 client_key               "#{current_dir}/client.pem"
-chef_server_url          "https://api.chef.io/organizations/#{username}-org"
+chef_server_url          "https://chefserver-01-01.#{ENV['ANIMAL']}.infralovers.academy/organizations/#{username}-org"
 cookbook_path            ["#{current_dir}/../cookbooks"]
